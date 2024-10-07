7c8f35daf9725a5cce3fa735f7379dedeefcd5b7
Viktor Virth <viktorvirth6@gmail.com>
2024-10-07 12:05:59 +0200

    Ctvrte cviceni

--- multiple_dice.py
+++ multiple_dice.py
@@ -1,8 +1,9 @@
 import random
 
 def roll_multiple_dice(n, k):
-    # Tvůj kód zde
-    pass
+    return random.randint(1, 6)
 
 # Otestování funkce
+print(roll_multiple_dice(3, 6))
+print(roll_multiple_dice(3, 6))
 print(roll_multiple_dice(3, 6))  # Simulace hodu 3 kostkami, každá má 6 hran
