a77eec829bca4352b9942caa16614a380477457b
Viktor Virth <viktorvirth6@gmail.com>
2024-10-07 11:31:54 +0200

    Prvni cviceni

--- biased_coin.py
+++ biased_coin.py
@@ -1,7 +1,7 @@
 import random
 
 def flip_biased_coin(p):
-    return random.randint(0,1)
+    return random.choice(["panna", "orel"])
 
 # Otestování funkce
 print(flip_biased_coin(0.7))  # Simulace hodu s 70% šancí na výhru
